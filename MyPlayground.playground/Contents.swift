e23fbfeddc38befd3f75d4d588fd57ca044eb60f
Luiz Henrique Monteiro de Carvalho <lhmcarvx5@gmail.com>
2020-04-30 15:18:02 -0300

    alteracoes com TRATOR

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,10 +4,12 @@ class VendingMachineProduct {
     var name: String
     var amount: Int
     var price: Double
-    init(name: String, amount: Int, price: Double) {
+    var nivelDeTrator: String
+    init(name: String, amount: Int, price: Double, nivelDeTrator: String) {
         self.name = name
         self.amount = amount
         self.price = price
+        self.nivelDeTrator = nivelDeTrator
     }
 }
 
@@ -17,6 +19,7 @@ enum VendingMachineError: Error {
     case productUnavailable
     case productStuck
     case insuficientFounds
+    case nivelDeTratorErrado
 }
 
 extension VendingMachineError: LocalizedError {
@@ -30,6 +33,8 @@ extension VendingMachineError: LocalizedError {
             return "YOU NEED MORE MONEY"
         case .productUnavailable:
             return "ta unavaible"
+        case .nivelDeTratorErrado:
+            return "O SEU NIVEL DE TRATOR ESTÁ ERRADO REPENSE EM SUAS AÇÕES"
         }
     }
 }
@@ -41,10 +46,12 @@ class VendingMachine {
         self.money = 0
     }
     
-    func getProduct(named name: String, with money: Double) throws {
+    func getProduct(named name: String, with money: Double, with nivelDeTrator: String) throws {
         
         //TODO: receber o dinheiro e salvar em uma variável
         self.money += money
+        
+        
         
         //TODO: achar o produto que o cliente quer
         let produtoOptional = estoque.first { (produto) -> Bool in
@@ -52,11 +59,21 @@ class VendingMachine {
         }
         guard let produto = produtoOptional else { throw VendingMachineError.productNotFound  }
         
+        
+        
         //TODO: ver se ainda tem esse produto
         guard produto.amount > 0 else { throw VendingMachineError.productUnavailable}
         
         //TODO: ver se o dinheiro é o suficiente pro produto
         guard produto.price <= self.money else { throw VendingMachineError.insuficientFounds }
+        
+        
+        
+        
+        let produtoOptionalNivelTrator = estoque.first { (produto) -> Bool in
+            return produto.nivelDeTrator == nivelDeTrator
+        }
+        guard let produtoNT = produtoOptionalNivelTrator else { throw VendingMachineError.nivelDeTratorErrado  }
        
         
         
@@ -80,13 +97,13 @@ class VendingMachine {
 
 
 let vendingMachine = VendingMachine(products: [
-    VendingMachineProduct(name: "Carregador de iPhone", amount: 5, price: 150.00),
-    VendingMachineProduct(name: "Trator", amount: 1, price: 75000.00),
-    VendingMachineProduct(name: "Umbrela", amount: 10, price: 50.00)
+    VendingMachineProduct(name: "Carregador de iPhone", amount: 5, price: 150.00, nivelDeTrator: "Quarenta"),
+    VendingMachineProduct(name: "Trator", amount: 1, price: 75000.00, nivelDeTrator: "Trinta"),
+    VendingMachineProduct(name: "Umbrela", amount: 10, price: 50.00, nivelDeTrator: "Vinte e cinto e meio")
 ])
 do {
-    try vendingMachine.getProduct(named: "Trator", with: 4000.0)
-    try vendingMachine.getProduct(named: "Umbrela", with: 200.0)
+    try vendingMachine.getProduct(named: "Trator", with: 75000.0, with: "Trintas")
+    try vendingMachine.getProduct(named: "Umbrela", with: 200.0, with: "Vinte e cinto e meio")
     print("deu bom")
 } catch VendingMachineError.productStuck{
     print("Pedimos desculpas, seu produto ta preso babaca.")
